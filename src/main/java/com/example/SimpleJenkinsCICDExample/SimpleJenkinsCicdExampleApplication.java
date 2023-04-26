b333c840316b9f5ad4e3fce69c24cc6b816f0c02
Jovana Zivkovic <116586798+jovanazivkovic99@users.noreply.github.com>
2023-04-26 16:54:08 +0200

    initial commit

---
+++ src/main/java/com/example/SimpleJenkinsCICDExample/SimpleJenkinsCicdExampleApplication.java
@@ -0,0 +1,13 @@
+package com.example.SimpleJenkinsCICDExample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleJenkinsCicdExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimpleJenkinsCicdExampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/SimpleJenkinsCICDExample/SimpleJenkinsCicdExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SimpleJenkinsCICDExample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleJenkinsCicdExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
