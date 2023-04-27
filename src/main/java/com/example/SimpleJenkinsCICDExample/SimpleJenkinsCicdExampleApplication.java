40df322200988c0fb51861c8f23543ce63e91c72
Jovana Zivkovic <116586798+jovanazivkovic99@users.noreply.github.com>
2023-04-27 12:05:07 +0200

    Added message to controller

--- src/main/java/com/example/SimpleJenkinsCICDExample/SimpleJenkinsCicdExampleApplication.java
+++ src/main/java/com/example/SimpleJenkinsCICDExample/SimpleJenkinsCicdExampleApplication.java
@@ -2,10 +2,17 @@ package com.example.SimpleJenkinsCICDExample;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SimpleJenkinsCicdExampleApplication {
 
+	@GetMapping
+	public String message() {
+		return "Poruka iz kontrolera";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleJenkinsCicdExampleApplication.class, args);
 	}
